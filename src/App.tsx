1aeae903a44fd8c2dcb52a8feb01196963e02c3f
Bill Fugina <bill@dogspots.com>
2019-03-16 16:40:01 -0400

    add imn logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { Heading } from '@inmotionnow/momentum-components-react'
+import { Logo } from './components/logo'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Logo />
         <Heading>Hello World</Heading>
       </div>
     )
---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import './components.css'
+import logo from '../images/inMotion_ignite-color.78e7a674.svg'
+
+const Logo = () => {
+  return <div className='logo' />
+}
+
+export { Logo }
